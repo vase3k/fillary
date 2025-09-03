3a761923689fd7fc6c488ae524456d7993ca8194
Vasiliy Vinogradov <vase3k@gmail.com>
2025-09-03 10:50:06 +0300

    events sliders

--- src/js/modules/swiper.js
+++ src/js/modules/swiper.js
@@ -13,4 +13,12 @@ const swiper = new Swiper('.mySwiper', {
     initialSlide: 2,
 });
 
-export default swiper;
+const swiperPromo = new Swiper('.events__swiper', {
+    modules: [Navigation],
+    slidesPerView: 1,
+    loop: true,
+    navigation: {
+        nextEl: '.events__swiper .custom-next',
+        prevEl: '.events__swiper .custom-prev',
+    },
+});
